9ac3e9d96648fd0a902505e8cf7ee56938b2a934
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-08 14:19:56 +0200

    bootstraped

--- app/index.php
+++ app/index.php
@@ -3,11 +3,23 @@
 header('Content-type: text/html; charset=UTF-8');
 
 
-echo "<html><body>\n";
+echo "<html>
 
+    <head><!-- Latest compiled and minified CSS -->
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' integrity='sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS' crossorigin='anonymous'></script>
+
+    </head>
+
+    <body>\n";
+
+    echo "<div class='container-fluid'>\n";
 
 if ($ver = $_GET['ver']) {
 
+    echo "<div class='row'>\n";
     echo "<h1>Installation Version $ver</h1>\n";
 
     echo '<pre>';
@@ -26,10 +38,15 @@ if ($ver = $_GET['ver']) {
 
     echo '<h2>Installation finished! Open your webbrowser and have fun!</h2>';
 
+    echo "</div>";
 
 } else {
 
+    echo "<div class='row'>\n";
+    echo "<div class='col-xs-12'>\n";
     echo "<h1>Select version</h1>\n";
+    echo "</div>";
+
 
     // run list
     $output = null;
@@ -43,13 +60,16 @@ if ($ver = $_GET['ver']) {
         foreach ($output as $tag) {
             $string = strstr($tag, 'tags/');
             $ver = str_replace("tags/", "", $string);
-            echo '<p><a href="index.php?ver='.$ver.'">Version '.$ver.'</a></p>';
+            echo '<div class="col-xs-12 col-sm-6 col-md-3"><p><a class="btn btn-primary" href="index.php?ver='.$ver.'">Version '.$ver.'</a></p></div>';
         }
     } else {
         echo "Invalid list output";
     }
+
+    echo "</div>";
 }
 
 
 
+    echo "</div>";
 echo "</body></html>";
